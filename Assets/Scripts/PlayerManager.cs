44c60cf910d55977499a9c1b857757813be1064c
Ethan <136783056+Ethan-GameDevelopment@users.noreply.github.com>
2023-09-20 15:36:51 +1000

    UI Works
    + TMPUGUI for health and points

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -1,12 +1,16 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
     public int playerHP = 10;
     public int playerPoints;
-    
+
+    public TextMeshProUGUI playerHealthDisplay;
+    public TextMeshProUGUI playerPointsDisplay;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +20,8 @@ public class PlayerManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        playerHealthDisplay.text = "♥ | " + playerHP + "/ 10";
+        playerPointsDisplay.text = "★ | " + playerPoints;
     }
 
     public void TakeDamage()
