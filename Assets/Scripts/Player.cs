cd1b307825d2ff562d0f63ef5cbb22808f3a05e3
Ethan <136783056+Ethan-GameDevelopment@users.noreply.github.com>
2023-10-04 10:45:57 +1100

    Individual Player UI
    + HP Slider
    + 2 Players Hardcoded UI

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public GameManager gameManager;
+    
     public string playerName;
     
     public int hp;
@@ -19,7 +21,10 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (hp < 1)
+        {
+            Death();
+        }
     }
 
     public void TakeDamage()
@@ -30,5 +35,11 @@ public class Player : MonoBehaviour
     public void AddPoints()
     {
         points += 1;
+    }
+
+    public void Death()
+    {
+        print("You died.");
+        
     }
 }
--- Assets/Scripts/PlayerUIPanel.cs
+++ Assets/Scripts/PlayerUIPanel.cs
@@ -1,28 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.UIElements;
+using UnityEngine.UI;
+
 
 public class PlayerUIPanel : MonoBehaviour
 {
+    public GameObject gameManager;
     public GameObject player;
     //public GameObject gameManager;
-    
+
     public TextMeshProUGUI playerName;
     public TextMeshProUGUI currentHp;
     public TextMeshProUGUI score;
 
     public Slider HpSlider;
-    
-    
-    
-    
+
+
+
+
     // Start is called before the first frame update
     void Start()
     {
         playerName.text = player.GetComponent<Player>().playerName;
-        
+
     }
 
     // Update is called once per frame
@@ -30,5 +33,7 @@ public class PlayerUIPanel : MonoBehaviour
     {
         currentHp.text = "♥ " + player.GetComponent<Player>().hp.ToString();
         score.text = player.GetComponent<Player>().points.ToString() + " ♦";
+        HpSlider.value = player.GetComponent<Player>().hp;
+        HpSlider.maxValue = gameManager.GetComponent<GameManager>().maxHealth;
     }
 }
